f5cb3cdf787fe0a71258484b3a899f0bb34ae077
hyejin1231 <phj981231@naver.com>
2021-03-18 15:30:10 +0900

    second commint 혜진 2021.03.18.목 응용SW연동 간단한 계산기 만들어보기~~

---
+++ jspbook/src/jspbook/ch04/CalcServlet.java
@@ -0,0 +1,100 @@
+package jspbook.ch04;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+@WebServlet("/CalcServlet")
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		doGet(request, response);
+		
+		int num1, num2;
+		int result; 
+		String op;
+		
+		PrintWriter out = response.getWriter();
+		
+		num1 = Integer.parseInt(request.getParameter("num1"));
+		num2 = Integer.parseInt(request.getParameter("num2"));
+		op = request.getParameter("operator");
+		
+		result = calc(num1, num2, op);
+		
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>계산기</TITLE></HEAD>");
+		out.println("<BODY>");
+		out.println("<div style=\"text-align: center;\">");
+		out.println("<H2>계산결과</H2>");
+		out.println("<HR>");
+		out.println(num1 + " " + op + " " + num2 + " = " + result );
+		out.println("</div>");
+		out.println("</BODY></HTML>");
+		
+	}
+
+	public int calc(int num1, int num2, String op) {
+		int result = 0;
+		
+		if(op.equals("+")) {
+			result = num1 + num2;
+		}else if(op.equals("-")) {
+			result = num1 - num2;
+		}else if(op.equals("*")) {
+			result = num1 * num2;
+		}else if(op.equals("/")){
+			result = num1 / num2;
+		}
+		return result;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
